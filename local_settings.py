391ba9d3f3d247c605a9a0f2a762af34b6a6c01f
Qwlouse <klaus.greff@gmx.net>
2012-12-15 17:57:54 +0100

    added more documentation

--- local_settings.py
+++ local_settings.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # coding=utf-8
 
-# here you can overwrite some of the settings from settings.py
-# this file will be ignored by git
+# Here you can overwrite some of the settings from settings.py.
+# Note: This file will be ignored by git.
 
 ADMIN_PASS = "1234"
